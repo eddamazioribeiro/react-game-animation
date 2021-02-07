3bb953b48d2d679fefa4d2d716dd24e28122a3b9
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2021-02-07 20:46:54 -0300

    Added game loop timer

--- src/Game.js
+++ src/Game.js
@@ -1,18 +1,34 @@
-import React, {useRef} from 'react';
+import React, {useRef, useEffect, useState} from 'react';
 
 const Game = ({height, width, tilesize}) => {
-  const gameScreen = useRef();
-  const config = {
+  const _gameScreen = useRef();
+  const _config = {
     height: height,
     width: width,
-    tilesize: tilesize
+    tilesize: tilesize,
+    gameSpeed: 1000
+  };
+  var _ticker = null;
+
+  const [gameTimer, setGameTimer] = useState(0);
+
+  useEffect(() => {
+    initGame();
+  }, []);
+
+  let t = 0;
+  const initGame = () => {
+    _ticker = setInterval(() => {
+      setGameTimer(t++);
+      console.log(t);
+    }, _config.gameSpeed);
   };
 
   return(
     <div>
-    <canvas ref={gameScreen}
-        height={config.height * tilesize}
-        width={config.width * tilesize}
+    <canvas ref={_gameScreen}
+        height={_config.height * _config.tilesize}
+        width={_config.width * _config.tilesize}
         style={{
           border: '1px solid black',
           background: 'DimGrey'
